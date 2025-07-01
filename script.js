05310d90969d913eaee7c33e44feb676a76edd31
Jyotsna@1902 <jyotsnab1902@gmail.com>
2025-07-01 08:46:35 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let age = 7
-if (age<18) {
-    console.log(not eligible to vote)
-    else{
-        eligible to vote
-    }
-    
+// let username = prompt("enter your username");
+// let password = prompt("enter your password");
+// if(username== "jyotsna"&& password == "12345"){
+//     console.log("welcome to instagram")
+// }else{
+// //     console.log("invalid credentials")
+// // }
+// let math = 27;
+// let social = 32;
+// let science = 23;
+// let telugu = 46;
+// let english = 29;
+
+// if(math > 35 || social > 35 || science > 35 || telugu > 35 || english > 35 ){
+//    console.log("you got the cycle");
+// }
+   
+let marks = 79
+if(marks>70){
+   console.log("exellent")
+}else if(marks>60&&marks<70){
+   console.log("good")
+}else if(marks < 60 ){
+   console.log("average")
+}else{
+   console.log("invalid")
 }
+   
